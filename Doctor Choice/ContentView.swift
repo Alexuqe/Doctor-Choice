62b65052fe8db2ac723b50952a2df1805c84f4fe
alex <hordiah1@google.com>
2026-02-16 18:40:09 +0300

    Initial Commit

---
+++ Doctor Choice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Doctor Choice
+//
+//  Created by Sasha on 16.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Doctor Choice/Doctor_ChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctor_ChoiceApp.swift
+//  Doctor Choice
+//
+//  Created by Sasha on 16.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Doctor_ChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
